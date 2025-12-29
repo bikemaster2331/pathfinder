15644fa511839b7687dfaba14f551537fd987fbe
Marthan Lanuzga <tanlanuzga@gmail.com>
2025-12-29 22:44:51 +0800

    pages

--- src/frontend/pages/Itinerary.jsx
+++ src/frontend/pages/Itinerary.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/itinerary_page/Itinerary.module.css';
+import PreferenceCard from '../components/itineraryCard';
 
-export default function Itinerary(){
+
+export default function ItineraryPage() {
+    const navigate = useNavigate();
+    
     return (
-        <h1>fuck you</h1>
+        <div className={styles.itineraryContainer}>
+            <div className={styles.mapBackground}>
+                {/*Dito yung map later*/}
+            </div>
+            <PreferenceCard />
+        </div>
     );
 }
+
+
+// ETO YUNG BACKGROUND
