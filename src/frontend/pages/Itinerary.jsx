3d9f090b0144400a9e317a47b62b5aaa6a4df3fe
Marthan Lanuzga <tanlanuzga@gmail.com>
2025-12-31 19:33:50 +0800

    pages

--- src/frontend/pages/Itinerary.jsx
+++ src/frontend/pages/Itinerary.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import styles from '../styles/itinerary_page/Itinerary.module.css';
 import PreferenceCard from '../components/itineraryCard';
-
+import MapBackground from '../components/map';
 
 export default function ItineraryPage() {
-    const navigate = useNavigate();
+    const [selectedActivities, setSelectedActivities] = useState({
+        Swimming: false,
+        Hiking: false,
+        Dining: false,
+        Sightseeing: false,
+        Photography: false,
+        Shopping: false,
+        Accommodation: false
+    });
+
+    useEffect(() => {
+        fetch('/catanduanes_full.geojson')
+            .then(res => res.json())
+            .then(data => {
+                setAllSpots(data);
+            })
+            .catch(err => console.error("Error loading data:", err));
+    }, []);
     
     return (
         <div className={styles.itineraryContainer}>
-            <div className={styles.mapBackground}>
-                {/*Dito yung map later*/}
-            </div>
-            <PreferenceCard />
+            <MapBackground selectedActivities={selectedActivities}
+            allSpots={allSpots} />
+            <PreferenceCard 
+                selectedActivities={selectedActivities}
+                setSelectedActivities={setSelectedActivities}
+                allSpots={allSpots}
+            />
         </div>
     );
 }
-
-
-// ETO YUNG BACKGROUND
