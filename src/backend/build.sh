da1da5ed1ac8db3e84c561fdeb1be9ae7d08b038
Marthan Lanuzga <tanlanuzga@gmail.com>
2026-01-21 04:21:02 +0800

    new ud

--- src/backend/build.sh
+++ src/backend/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
-echo "Installing CPU Torch with high timeout..."
-pip install torch==2.1.2+cpu --index-url https://download.pytorch.org/whl/cpu --default-timeout=1000
+echo "1. Installing CPU Torch (High Timeout Mode)..."
+# UPDATED: Changed version to 2.6.0+cpu to match Python 3.13 support
+pip install torch==2.6.0+cpu --index-url https://download.pytorch.org/whl/cpu --default-timeout=1000
 
-echo "Installing remaining requirements..."
-# Since build.sh and requirements.txt are now in the same folder:
+echo "2. Installing remaining packages..."
 pip install -r requirements.txt
