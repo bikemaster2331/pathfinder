0f7dfcfb342bfcf5442a8906b755ba63e96daa84
Marthan Lanuzga <tanlanuzga@gmail.com>
2026-02-02 03:07:00 +0800

    navbar

---
+++ src/frontend/components/toolbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from '../styles/itinerary_page/toolbar.module.css';
+
+export default function FloatingToolbar() {
+    return (
+        <div className={styles.toolbarContainer}>
+            
+            {/* 1. LEFT GROUP: Brand / Logo */}
+            <div className={styles.leftGroup}>
+                <div className={styles.brandBox}>
+                    <div className={styles.logoCircle}>
+                        {/* YOUR CUSTOM LOGO SVG */}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256">
+                            <path d="M232,80,208,96v24a96,96,0,0,1-96,96H24a8,8,0,0,1-6.25-13L104,99.52V76.89c0-28.77,23-52.75,51.74-52.89a52,52,0,0,1,50.59,38.89Z" opacity="0.2"></path>
+                            <path d="M176,68a12,12,0,1,1-12-12A12,12,0,0,1,176,68Zm64,12a8,8,0,0,1-3.56,6.66L216,100.28V120A104.11,104.11,0,0,1,112,224H24a16,16,0,0,1-12.49-26l.1-.12L96,96.63V76.89C96,43.47,122.79,16.16,155.71,16H156a60,60,0,0,1,57.21,41.86l23.23,15.48A8,8,0,0,1,240,80Zm-22.42,0L201.9,69.54a8,8,0,0,1-3.31-4.64A44,44,0,0,0,156,32h-.22C131.64,32.12,112,52.25,112,76.89V99.52a8,8,0,0,1-1.85,5.13L24,208h26.9l70.94-85.12a8,8,0,1,1,12.29,10.24L71.75,208H112a88.1,88.1,0,0,0,88-88V96a8,8,0,0,1,3.56-6.66Z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            {/* 2. RIGHT GROUP: Navigation Links */}
+            <div className={styles.centerGroup}>
+                <button className={styles.navLink}>About</button>
+                <button className={styles.navLink}>Creators</button>
+                <button className={styles.navLinkHighlight}>Contact Us</button>
+            </div>
+
+        </div>
+    );
+}
